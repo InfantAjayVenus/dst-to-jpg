6a3fd1dcdf2cccff422b682cbe42f6f72e176d19
Infant Ajay Venus <infantcsajay@outlook.com>
2023-10-24 18:37:00 +0530

    clean directory

--- rename.py
+++
@@ -1,7 +0,0 @@
-import os
-
-with open('paths.txt', 'r') as file:
-    file_paths = file.read().splitlines()
-    for path in file_paths:
-        if path.endswith('.DST'):
-            os.rename(path, path.replace('.DST', '.dst'))
